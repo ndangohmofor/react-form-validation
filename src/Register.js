9dc8938f28f8fc7ee50f435cfe6f5433516a7eed
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-11 23:36:16 -0400

    Register component - added name, password, amd confirm password fields

---
+++ src/Register.js
@@ -0,0 +1,33 @@
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+import React from "react";
+
+const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatchPwd, setValidMatchPwd] = useState(false);
+  const [matchPwdFocus, setMatchPwdFocus] = useState(false);
+
+  return <div></div>;
+};
+
+export default Register;
