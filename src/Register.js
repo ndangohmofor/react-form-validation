c2ea8eff3aec5da12f854f8f2f75f7919b935600
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-30 18:11:22 -0400

    installed axios and created a base axios instance

--- src/Register.js
+++ src/Register.js
@@ -5,6 +5,7 @@ import {
   faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "./api/axios";
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
