6ebd77779e20d98aa6a2ad98886902d0c43ae2f1
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-02 20:02:07 -0400

    Started the login component

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  return <div></div>;
+};
+
+export default Login;
