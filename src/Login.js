45171dd02847a52c7e1827e0570bdd31bf9a1d83
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-02 20:17:20 -0400

    added the form elements and inputs

--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,4 @@
+import { text } from "@fortawesome/fontawesome-svg-core";
 import React, { useEffect, useRef, useState } from "react";
 
 const Login = () => {
@@ -13,7 +14,51 @@ const Login = () => {
     userRef.current.focus();
   }, []);
 
-  return <div></div>;
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, password]);
+
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Sign In</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={user}
+          required
+        />
+        <button>Sign in</button>
+      </form>
+      <p>
+        Need an Account?
+        <br />
+        <span className="line">
+          {/* put a router link here */}
+          <a href="#">Sign Up</a>
+        </span>
+      </p>
+    </section>
+  );
 };
 
 export default Login;
