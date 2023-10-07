4e0e74ffc150f2dce68be36f98d19df7f7d64fd9
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-07 08:03:47 -0400

    Updated the project and added a layout component for better layout and structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import Layout from "./Layout";
 import Login from "./Login";
 import Register from "./Register";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { Routes, Route } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="App">
-        {/* <Register /> */}
-        <Login />
-      </main>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+
+          {/* protected routes */}
+          <Route path="admin" element={<Admin />} />
+          <Route path="employee" element={<Employee />} />
+
+          {/* catch all  */}
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
     </QueryClientProvider>
   );
 }
