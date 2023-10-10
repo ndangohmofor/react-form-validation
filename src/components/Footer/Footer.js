a67707bf8b9a74a618b944ad52c4739da708783d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-10 15:11:18 -0400

    Updated the footer for better styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,72 +1,104 @@
 import React from "react";
 import { Navbar, Nav, Row, Col, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary footer">
+      <footer expand="lg" className="bg-body-tertiary footer">
         <Container>
           <Row>
-            <Col className="justify-content-right">
-              <Navbar.Brand href="/">
+            <Col className="justify-content-right" md={4}>
+              <div className="d-flex align-items-left mb-3">
                 <img
                   id="footer_logo"
                   alt="Workout Planner Logo"
                   src="images/Logo.png"
                   width={"40"}
                   height={"40"}
+                  className="me-2"
                 />{" "}
-                WorkoutPlanner
-              </Navbar.Brand>
+                <h5 className="footer">WorkoutPlanner</h5>
+              </div>
               <p className="footer-paragraph">
                 WorkoutPlanner is a leader in establishing healthy routines and
                 lifestyles for our members.
               </p>
             </Col>
-          </Row>
-        </Container>
-        <Container>
-          <Row>
-            <Col className="justify-content-center">
-              <Navbar.Text href="/">Offerings</Navbar.Text>
-              <Nav.Link>Pool</Nav.Link>
-              <Nav.Link>Pilates</Nav.Link>
-              <Nav.Link>Yoga</Nav.Link>
+            <Col md={2} className="justify-content-center">
+              <h5 className="footer">Our Offerings</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#!">Pool</a>
+                </li>
+                <li>
+                  <a href="#!">Pilates</a>
+                </li>
+                <li>
+                  <a href="#!">Yoga</a>
+                </li>
+              </ul>
             </Col>
-          </Row>
-        </Container>
-        <Container>
-          <Row>
             <Col className="justify-content-right">
-              <Navbar.Text href="/">Useful Links</Navbar.Text>
-              <Nav.Link>Upcoming Classes</Nav.Link>
-              <Nav.Link>Workout Resources</Nav.Link>
-              <Nav.Link>Your account</Nav.Link>
+              <h5 className="footer">Useful Links</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#!">Upcoming Classes</a>
+                </li>
+                <li>
+                  <a href="#!">Workout Resources</a>
+                </li>
+                <li>
+                  <a href="#!">Talk To An Instructor</a>
+                </li>
+              </ul>
             </Col>
-          </Row>
-        </Container>
-        <Container>
-          <Row>
+            <Col md={3} className="justify-content-right">
+              <h5 className="footer">Contact Us</h5>
+              <p>
+                123 S Main St
+                <br />
+                New York, NY 10012, US
+                <br />
+                Tel: (123) 456 7890
+              </p>
+            </Col>
             <Col className="justify-content-center">
-              <Navbar.Text href="/">Help</Navbar.Text>
-              <Nav.Link>Office hours</Nav.Link>
-              <Nav.Link>Member Benefits</Nav.Link>
-              <Nav.Link>Careers</Nav.Link>
+              <h5 className="footer">Follow Us</h5>
+              <ul className="list-unstyled my-0">
+                <li>
+                  <a href="#!">
+                    <FontAwesomeIcon icon={faFacebook} /> Facebook
+                  </a>
+                </li>
+                <li>
+                  <a href="#!">
+                    <FontAwesomeIcon icon={faTwitter} /> Twitter
+                  </a>
+                </li>
+                <li>
+                  <a href="#!">
+                    <FontAwesomeIcon icon={faInstagram} /> Instagram
+                  </a>
+                </li>
+              </ul>
             </Col>
           </Row>
         </Container>
+      </footer>
+      <div className="text-center bg-primary">
         <Container>
-          <Row>
-            <Col className="justify-content-right">
-              <Navbar.Text href="/">Contacts</Navbar.Text>
-              <Nav.Link>123 S Main St</Nav.Link>
-              <Nav.Link>New York, NY 10012, US</Nav.Link>
-              <Nav.Link>(123) 456 7890</Nav.Link>
-            </Col>
-          </Row>
+          <p className="my-0 footer-copyright">
+            &copy; {new Date().getFullYear()} Workout Planner
+          </p>
         </Container>
-      </Navbar>
+      </div>
     </>
   );
 };
