2b8b6cc9f3b7eeafafb55bacf4aa0d210d5ca2b6
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-19 23:53:36 -0400

    Added recommended headers to the axios instance

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,4 +2,10 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:8080",
+  headers: {
+    "Content-Type":
+      "application/json, application/x-www-form-urlencoded, multipart/form-data",
+    Accept: "application/json, plain/text, application/x-www-form-urlencoded",
+    "Cache-Control": "no-cache",
+  },
 });
