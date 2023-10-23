a28ba4ef7800f14f9bfd7c4b921f930525a61a36
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-22 21:10:40 -0400

    Updated the axios instance and added default settings to axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:8080";
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 const HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json, plain/text, application/x-www-form-urlencoded",
@@ -10,6 +13,7 @@ const HEADERS = {
 export default axios.create({
   baseURL: BASE_URL,
   headers: HEADERS,
+  withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
