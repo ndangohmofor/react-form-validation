a7f9aeb6c91ea41c1862796cd4cf5f84bb82a453
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-04 15:50:44 -0400

    removed the auth header from the axios as refresh token from cookie is used for generating a new token. updated setAuth to use the body of the response to extract role, token and username

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,19 +1,23 @@
 import axios from "../api/axios";
 import useAuth from "./useAuth";
 
-const useRefreshToken = (props) => {
+const useRefreshToken = () => {
   const { auth, setAuth } = useAuth();
   const refresh = async () => {
     const response = await axios.get("/api/v1/refreshtoken", {
-      headers: {
-        Authorization: `Bearer ${auth.accessToken}`,
-      },
       withCredentials: true,
     });
-
     setAuth((prev) => {
-      return { ...prev, accessToken: response.data.body.access_token };
+      console.log(JSON.stringify("prevstate: ", prev));
+      console.log("response body", response.data.body);
+      return {
+        ...prev,
+        role: response.data.body.role,
+        username: response.data.body.username,
+        accessToken: response.data.body.access_token,
+      };
     });
+    console.log("auth: ", auth);
     return response.data.body.access_token;
   };
   return refresh;
