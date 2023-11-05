3919553ae95316778fc29236aa958f0036e0dd74
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-05 18:32:59 -0500

    Added todos in the useRefreshToken hook

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -3,6 +3,7 @@ import useAuth from "./useAuth";
 
 const useRefreshToken = () => {
   const { auth, setAuth } = useAuth();
+  //TODO: Update this hook to use react-query's useQuery hook so that it handles instances of multiple calls seamlessly.
   const refresh = async () => {
     const response = await axios.get("/api/v1/refreshtoken", {
       withCredentials: true,
