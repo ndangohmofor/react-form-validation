afc868c73e005c12943e5f866896bc2536b6d8b1
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-05 13:46:52 -0500

    updated the users query to use useQuery

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,54 +1,53 @@
 import { useState, useEffect, useRef } from "react";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 const Users = () => {
   const effectRan = useRef(false);
 
-  const [users, setUsers] = useState();
+  // const [users, setUsers] = useState();
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const location = useLocation();
 
-  useEffect(() => {
-    let isMounted = true;
-    const controler = new AbortController();
+  let isMounted = true;
+  const controller = new AbortController();
 
-    if (effectRan.current === true) {
-      const getUsers = async () => {
-        try {
-          const response = await axiosPrivate.get("/api/v1/admin/allusers", {
-            signal: controler.signal,
-          });
-          isMounted && setUsers(response.data);
-        } catch (err) {
-          console.error(err);
-          navigate("/login", { state: { from: location }, replace: true });
-        }
-      };
+  const getAllUsers = () => {
+    return axiosPrivate.get("/api/v1/admin/allusers", {
+      signal: controller.signal,
+    });
+  };
 
-      getUsers();
-    }
-    return () => {
+  const { data: users, isLoading } = useQuery(["allUsers"], getAllUsers, {
+    onError: (error) => {
+      console.log(error);
+      navigate("/login", { state: { from: location }, replace: true });
+    },
+    onSuccess: () => {
       isMounted = false;
-      controler.abort();
+      controller.abort();
       effectRan.current = true;
-    };
-  }, []);
+    },
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
     <article>
       <h2>Users List</h2>
-      {users?.length ? (
+      {users?.data.length ? (
         <ul>
-          {users.map((user, i) => (
+          {users.data.map((user, i) => (
             <li key={i}>{user?.username}</li>
           ))}
         </ul>
       ) : (
         <p>No users to display</p>
       )}
-      <br />
     </article>
   );
 };
