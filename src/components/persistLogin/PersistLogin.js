9a89a2a87c6b6e1ac850f7b1cd15d73d3e33a96c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-06 23:15:13 -0500

    used the persist state variable to determine when to enable isLoading in the persist component or just to render the children

--- src/components/persistLogin/PersistLogin.js
+++ src/components/persistLogin/PersistLogin.js
@@ -6,19 +6,21 @@ import useAuth from "../../hooks/useAuth";
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   const refresh = useRefreshToken();
-  const { auth } = useAuth();
+  const { auth, persist } = useAuth();
 
   useEffect(() => {
+    let isMounted = true;
     const verifyRefreshToken = async () => {
       try {
         await refresh();
       } catch (err) {
         console.error(err);
       } finally {
-        setIsLoading(false);
+        isMounted && setIsLoading(false);
       }
     };
     !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+    return () => (isMounted = false);
   }, []);
 
   useEffect(() => {
@@ -26,7 +28,9 @@ const PersistLogin = () => {
     console.log(`authToken: (${JSON.stringify(auth?.accessToken)})`);
   }, [isLoading]);
 
-  return <>{isLoading ? <p>Loading...</p> : <Outlet />}</>;
+  return (
+    <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
+  );
 };
 
 export default PersistLogin;
