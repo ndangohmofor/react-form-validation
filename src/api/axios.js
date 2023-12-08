d85b37190938c188f352103ac3c3ca0834bce523
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-07 21:02:22 -0500

    updated axios to accept more media types

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:8080";
 axios.defaults.withCredentials = true;
+axios.defaults.withXSRF = true;
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 const HEADERS = {
   "Content-Type": "application/json",
-  Accept: "application/json, plain/text, application/x-www-form-urlencoded",
+  Accept:
+    "application/json, application/json-patch+json, plain/text, application/x-www-form-urlencoded",
   "Cache-Control": "no-cache",
 };
 
