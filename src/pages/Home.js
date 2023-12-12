f16bf2c02a1382fc4166c6ccee920ef3c325cdd0
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-11 22:11:39 -0500

    Added sections on the home page to enable snap-scrolling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 const Home = () => {
   return (
     <>
-      <div>
-        <h1>I am on the Home page</h1>
+      <div className="video-section scroll-container">
+        <section className="scroll-section">
+          <video
+            width={"100%"}
+            height={"auto"}
+            preload={"auto"}
+            autoPlay
+            muted
+            loop
+          >
+            <source
+              src="/videos/gym_ad_portrait_optimized.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </section>
       </div>
     </>
   );
