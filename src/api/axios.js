71bcf749f82888d208b8fc5dddd9fb0d0eff1443
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-17 16:38:55 -0500

    Updated the axios endpoint to use the ip address instead of localhost so that site can be access from any system

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://192.168.1.132:8080";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRF = true;
 axios.defaults.xsrfCookieName = "csrftoken";
