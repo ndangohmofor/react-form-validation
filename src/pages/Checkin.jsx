19a66cc14a6cbf77ff759b6473f6ddc0784dcfd6
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-06 18:27:25 -0500

    Added card to checkin to a workout session

--- src/pages/Checkin.jsx
+++ src/pages/Checkin.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const Checkin = () => {
-  return <div>Check In To the Gym</div>;
+  return (
+    <div className="row">
+      <Card style={{ width: "80vw" }}>
+        <Card.Header>Check in</Card.Header>
+        <Card.Body>
+          <Card.Title>Check in to your workout session</Card.Title>
+          <Card.Text>
+            You are not currently Checked into a workout session. Click the
+            button to Check in
+          </Card.Text>
+          <Button variant="primary" size="sm">
+            Checkin
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Checkin;
