b7a42f086a53ca02ed6e38be70a88d3f15b1c266
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-07 00:29:20 -0500

    added button for checkin and checkout and introduce auth object with checkedin status

--- src/pages/Checkin.jsx
+++ src/pages/Checkin.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import useAuth from "../hooks/useAuth";
 
 const Checkin = () => {
+  const { setAuth, auth } = useAuth();
+  const axios = useAxiosPrivate();
+  const handleCheckin = async () => {
+    await axios.post("/api/v1/checkins/usercheckin");
+  };
+
   return (
     <div className="row">
-      <Card style={{ width: "80vw" }}>
-        <Card.Header>Check in</Card.Header>
-        <Card.Body>
-          <Card.Title>Check in to your workout session</Card.Title>
-          <Card.Text>
-            You are not currently Checked into a workout session. Click the
-            button to Check in
-          </Card.Text>
-          <Button variant="primary" size="sm">
-            Checkin
-          </Button>
-        </Card.Body>
-      </Card>
+      {auth.checkedIn ? (
+        <Card style={{ width: "80vw" }}>
+          <Card.Header>Check out</Card.Header>
+          <Card.Body>
+            <Card.Title>Checkout to end workout sessioin</Card.Title>
+            <Card.Text>Click the button to Check out</Card.Text>
+            <Button onClick={handleCheckin} variant="primary" size="sm">
+              Checkout
+            </Button>
+          </Card.Body>
+        </Card>
+      ) : (
+        <Card style={{ width: "80vw" }}>
+          <Card.Header>Check in</Card.Header>
+          <Card.Body>
+            <Card.Title>Check in to your workout session</Card.Title>
+            <Card.Text>
+              You are not currently Checked into a workout session. Click the
+              button to Check in
+            </Card.Text>
+            <Button onClick={handleCheckin} variant="primary" size="sm">
+              Checkin
+            </Button>
+          </Card.Body>
+        </Card>
+      )}
     </div>
   );
 };
